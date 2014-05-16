c8f62f459dfb4d754858815901cb53e40f8bce98
Michael Hendricks <michael@ndrix.org>
2014-05-16 14:44:46 -0600

    Release v0.0.1

--- pack.pl
+++ pack.pl
@@ -1,11 +1,12 @@
 name(sweet).
 
-title('').
-version('0.0.1').
+title('Sugar to make Prolog a little sweeter').
 
 requires(lambda).
 
+version('0.0.1').
 download('http://packs.ndrix.com/sweet/sweet-0.0.1.tgz').
+
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
